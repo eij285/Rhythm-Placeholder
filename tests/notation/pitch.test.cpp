657b83d2c8490935b428a93dde60ca88ea0f818f
Elijah Lee <eij285@gmail.com>
2025-09-17 14:30:31 +1000

    Added test for pitch

---
+++ tests/notation/pitch.test.cpp
@@ -0,0 +1,131 @@
+#include <catch2/catch_test_macros.hpp>
+#include "notation/pitch.hpp"
+
+TEST_CASE("Test for constructor with two arguments") {
+    auto const C4 = notation::Pitch(notation::PitchName::C, 4);
+
+    CHECK(C4.name_ == notation::PitchName::C);
+    CHECK(C4.octave_ == 4);
+}
+
+TEST_CASE("Test for spaceship operator <=> and ==") {
+    SECTION("Equality == works for two pitches with same octave and PitchName") {
+        auto const Ds6_v1 = notation::Pitch(notation::PitchName::Ds, 6);
+        auto const Ds6_v2 = notation::Pitch(notation::PitchName::Ds, 6);
+
+        CHECK(Ds6_v1 == Ds6_v2);        
+    }
+
+    SECTION("Octave order produces correct ordering") {
+        auto const Bb2 = notation::Pitch(notation::PitchName::Bb, 2);
+        auto const Bb3 = notation::Pitch(notation::PitchName::Bb, 3);
+
+        CHECK((Bb2 <=> Bb3) == std::strong_ordering::less);
+
+        CHECK(Bb2 < Bb3);
+        CHECK(Bb2 <= Bb3);
+        CHECK(Bb3 > Bb2);
+        CHECK(Bb3 >= Bb2);
+    }
+
+
+}
+
+TEST_CASE("Test for is_enharmonic() member function") {
+    SECTION("Pitches separated by an octave are not enharmonic") {
+        auto const G2 = notation::Pitch(notation::PitchName::G, 2);
+        auto const G3 = notation::Pitch(notation::PitchName::G, 3);
+
+        CHECK_FALSE(G2.is_enharmonic(G3));
+    }
+
+    SECTION("Identically notated pitches are enharmonic") {
+        auto const As3_v1 = notation::Pitch(notation::PitchName::As, 3);
+        auto const As3_v2 = notation::Pitch(notation::PitchName::As, 3);
+
+        CHECK(As3_v1.is_enharmonic(As3_v1));
+        CHECK(As3_v1.is_enharmonic(As3_v2));
+    }
+
+    SECTION("Unrelated pitches are not enharmonic") {
+        auto const Bb2 = notation::Pitch(notation::PitchName::Bb, 2);
+        auto const Fs2 = notation::Pitch(notation::PitchName::Fs, 2);
+        auto const As5 = notation::Pitch(notation::PitchName::As, 5);
+
+        CHECK_FALSE(Bb2.is_enharmonic(Fs2));
+        CHECK_FALSE(Bb2.is_enharmonic(As5));
+        CHECK_FALSE(Fs2.is_enharmonic(As5));
+    }
+
+    SECTION("Pitches with differing notations but same absolute semitone value still match") {
+        auto const Cs3 = notation::Pitch(notation::PitchName::Cs, 3);
+        auto const Db3 = notation::Pitch(notation::PitchName::Db, 3);
+
+        CHECK(Cs3.is_enharmonic(Db3));
+        CHECK(Db3.is_enharmonic(Cs3));
+
+        auto const Es4 = notation::Pitch(notation::PitchName::Es, 4);
+        auto const F4 = notation::Pitch(notation::PitchName::F, 4);
+
+        CHECK(Es4.is_enharmonic(F4));
+        CHECK(F4.is_enharmonic(Es4));
+
+        auto const Bs5 = notation::Pitch(notation::PitchName::Bs, 5);
+        auto const C6 = notation::Pitch(notation::PitchName::C, 6);
+
+        CHECK(Bs5.is_enharmonic(C6));
+        CHECK(C6.is_enharmonic(Bs5));
+    }
+}
+
+TEST_CASE("Test for is_enharmonic() across an entire octave C to C") {
+    auto const B3 = notation::Pitch(notation::PitchName::B, 3);
+    auto const Cb4 = notation::Pitch(notation::PitchName::Cb, 4);
+
+    CHECK(B3.is_enharmonic(Cb4));
+
+    auto const Bs3 = notation::Pitch(notation::PitchName::Bs, 3);
+    auto const C4 = notation::Pitch(notation::PitchName::Ab, 4);
+
+    CHECK(Bs3.is_enharmonic(C4));
+
+    auto const Cs4 = notation::Pitch(notation::PitchName::Cs, 4);
+    auto const Db4 = notation::Pitch(notation::PitchName::Db, 4);
+
+    CHECK(Cs4.is_enharmonic(Db4));
+
+    auto const Ds4 = notation::Pitch(notation::PitchName::Ds, 4);
+    auto const Eb4 = notation::Pitch(notation::PitchName::Eb, 4);
+
+    CHECK(Ds4.is_enharmonic(Eb4));
+
+    auto const E4 = notation::Pitch(notation::PitchName::E, 4);
+    auto const Fb4 = notation::Pitch(notation::PitchName::Fb, 4);
+
+    CHECK(E4.is_enharmonic(Fb4));
+
+    auto const Es4 = notation::Pitch(notation::PitchName::Es, 4);
+    auto const F4 = notation::Pitch(notation::PitchName::F, 4);
+
+    CHECK(Es4.is_enharmonic(F4));
+
+    auto const Fs4 = notation::Pitch(notation::PitchName::Fs, 4);
+    auto const Gb4 = notation::Pitch(notation::PitchName::Gb, 4);
+
+    CHECK(Fs4.is_enharmonic(Gb4));
+
+    auto const Gs4 = notation::Pitch(notation::PitchName::Gs, 4);
+    auto const Ab4 = notation::Pitch(notation::PitchName::Ab, 4);
+
+    CHECK(Gs4.is_enharmonic(Ab4));
+
+    auto const As4 = notation::Pitch(notation::PitchName::As, 4);
+    auto const Bb4 = notation::Pitch(notation::PitchName::Bb, 4);
+
+    CHECK(As4.is_enharmonic(Bb4));
+
+    auto const Bs4 = notation::Pitch(notation::PitchName::Bs, 4);
+    auto const C5 = notation::Pitch(notation::PitchName::C, 5);
+
+    CHECK(Bs4.is_enharmonic(C5));
+}
