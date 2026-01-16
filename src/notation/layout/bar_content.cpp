a8d369a8dda9ac1dde33d4590b3e6ae40f953ce3
Elijah Lee <eij285@gmail.com>
2026-01-16 17:31:46 +1100

    Added implementation for bar_content (basics only)

---
+++ src/notation/layout/bar_content.cpp
@@ -0,0 +1,38 @@
+#include "notation/layout/bar_content.hpp"
+#include "notation/notation_globals.hpp"
+
+#include <cmath>
+
+namespace notation {
+    BarContent::BarContent(int total_duration)
+    : total_duration_{total_duration}, used_duration_{0} {};
+
+    auto BarContent::get_elements() const -> std::vector<std::unique_ptr<MusicalElement>> const& {
+        return elements_;
+    }
+
+    auto BarContent::remaining_duration() const -> int {
+        return total_duration_ - used_duration_;
+    }
+
+    auto BarContent::empty() const -> bool {
+        return elements_.empty();
+    }
+
+    auto BarContent::try_add(std::unique_ptr<MusicalElement> element) -> bool {
+        auto const elem_duration = element->get_duration();
+
+        if (remaining_duration() + dur_tolerance >= elem_duration) {
+            used_duration_ += elem_duration;
+            elements_.push_back(std::move(element));
+            return true;
+        }
+
+        return false;
+    }
+
+    auto BarContent::clear() -> void {
+        elements_.clear();
+        used_duration_ = 0;
+    }
+}
---
+++ src/notation/layout/score.cpp
@@ -0,0 +1,44 @@
+#include "notation/layout/score.hpp"
+
+namespace notation {
+    Score::Score(std::vector<Part> parts, std::string title, std::string author)
+    : title_{title}, author_{author}, parts_{parts} {};
+
+    Score::Score(std::string title, std::string author)
+    : Score::Score({}, title, author) {};
+
+    auto Score::get_title() const -> std::string {
+        return title_;
+    }
+
+    auto Score::get_author() const -> std::string {
+        return author_;
+    }
+
+    auto Score::get_timeline() const -> std::vector<BarInfo> const& {
+        return timeline_;
+    }
+
+    auto Score::get_parts() const -> std::vector<Part> const& {
+        return parts_;
+    }   
+
+    auto Score::add_new_bar() -> BarInfo& {
+        if (timeline_.empty()) {
+            // If there are no bars yet, add a default one
+            timeline_.emplace_back();
+
+        } else {
+            // Otherwise, copy the last bar's settings
+            BarInfo last_bar = timeline_.back();
+            timeline_.push_back(last_bar);
+        }
+
+        return timeline_.back();
+    }
+
+    auto Score::add_part(Part part) -> Part& {
+        parts_.push_back(part);
+        return parts_.back();
+    }   
+}
